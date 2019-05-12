57dfd82c9696a13ae9f08c5ee95f4dcf4255ab54
XIANGFANGCODE <fang.x@outlook.com>
2019-05-12 21:58:41 +0800

    order class

--- resources/order.py
+++ resources/order.py
@@ -6,6 +6,7 @@ class Order:
     def __init__(self, exchange, object, amount_of_object, price, amount_of_money, datetime):
         """
         订单类，描述如何向交易所下订单
+        注意order不是成交情况
         :param exchange: 交易所
         :param object: 标的
         :param amount_of_object: 买入标的数量
@@ -31,7 +32,15 @@ class Order:
         ))
 
     @staticmethod
-    def create_orders(account, signal):
+    def create_orders(mode, trade_limit, account, signal):
+        """
+        根据账户和信号创建订单
+        :param mode: 下单策略
+        :param trade_limit: 交易限制（只做多、只做空或者同时都能做）
+        :param account: 账户
+        :param signal: 信号
+        :return: 订单列表
+        """
         orders = list()
         order = Order('tushare', 'btc', '1', '5000', '5000', '20190501')
         order2 = Order('tushare', 'btc', '2', '5000', '10000', '20190502')
